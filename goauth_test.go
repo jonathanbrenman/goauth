35866c458d8e89107449b8080a279a4404bc29fd
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2023-03-07 12:16:57 -0500

    add unit test library

--- goauth_test.go
+++ goauth_test.go
@@ -1,55 +1,64 @@
 package goauth
 
 import (
-	"github.com/stretchr/testify/assert"
+	"errors"
 	"strings"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCreateTokenWithOutExpiration(t *testing.T) {
+	// A way to create a new instance of the assert library.
 	assert := assert.New(t)
-	goAuth := NewGoAuth("my-secret-key")
+	goAuthInstance := NewGoAuth("my-secret-key")
 
 	data := make(map[string]interface{})
-	data["hola"] = "Mundo"
-	data["pepe"] = 4587
+	data["value1"] = "Mama soy paquito"
+	data["value2"] = 4587
 
-	token, err := goAuth.CreateToken(data, 0)
+	const TimeOutToken = 3 * time.Second
+	token, err := goAuthInstance.CreateToken(data, TimeOutToken)
+
 	assert.NotEqual("", token)
 	assert.Equal(true, strings.Contains(token, "ey"))
 	assert.Nil(err)
 
-	claim, err := goAuth.DecryptToken(token)
-	assert.Nil(err)
+	claim, errDecrypt := goAuthInstance.DecryptToken(token)
+	assert.Nil(errDecrypt)
+	assert.Equal("Mama soy paquito", claim["value1"])
+	assert.Equal(float64(4587), claim["value2"])
 
-	assert.Equal("Mundo", claim["hola"])
-	assert.Equal(float64(4587), claim["pepe"])
 }
 
 func TestCreateTokenWithExpiration(t *testing.T) {
 	assert := assert.New(t)
-	goAuth := NewGoAuth("my-secret-key")
+	goAuthInstance := NewGoAuth("my-secret-key")
 
 	data := make(map[string]interface{})
-	data["myProperty1"] = "MyValue1"
-	data["myProperty2"] = 10.5
+	data["value1"] = "Mama soy paquito"
+	data["value2"] = 4587
 
-	token, err := goAuth.CreateToken(data, 10 * time.Minute)
+	const TimeOutToken = 1 * time.Second
+	token, err := goAuthInstance.CreateToken(data, TimeOutToken)
+
 	assert.NotEqual("", token)
 	assert.Equal(true, strings.Contains(token, "ey"))
 	assert.Nil(err)
 
-	claim, err := goAuth.DecryptToken(token)
-	assert.Nil(err)
-
-	assert.Equal("MyValue1", claim["myProperty1"])
-	assert.Equal(10.5, claim["myProperty2"])
+	time.Sleep(2 * time.Second)
+	claim, errDecrypt := goAuthInstance.DecryptToken(token)
+	assert.Equal(0, len(claim))
+	assert.NotNil(errDecrypt)
+	assert.Error(errors.New("Token is expired"), errDecrypt)
 }
 
 func TestDecryptTokenFail(t *testing.T) {
 	assert := assert.New(t)
-	goAuth := NewGoAuth("my-secret-key")
-	_, err := goAuth.DecryptToken("this_is_a_not_valid_token")
-	assert.Equal("token contains an invalid number of segments", err.Error())
+	goAuthInstance := NewGoAuth("my-secret-key")
+	claim, errDecrypt := goAuthInstance.DecryptToken("this_is_a_not_valid_token")
+	assert.Equal(0, len(claim))
+	assert.NotNil(errDecrypt)
+	assert.Equal("token contains an invalid number of segments", errDecrypt.Error())
 }
